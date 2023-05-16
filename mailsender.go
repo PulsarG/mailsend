8783acc80aeaf54f06c97bb7b4cf84f9c656dd5c
PulsarG <pulsar.g.projects@gmail.com>
2023-05-16 11:43:58 +0500

    for gmail and for other

--- mailsender.go
+++ mailsender.go
@@ -4,7 +4,7 @@ import (
 	"net/smtp"
 )
 
-func SendMail(mailFrom, mailTo, key string, err error) {
+func SendMailGoogle(mailFrom, mailTo, key string, err error) {
 	auth := smtp.PlainAuth(
 		"",
 		mailFrom,
@@ -16,6 +16,29 @@ func SendMail(mailFrom, mailTo, key string, err error) {
 
 	errSend := smtp.SendMail(
 		"smtp.gmail.com:587",
+		auth,
+		mailFrom,
+		[]string{mailTo},
+		[]byte(msg),
+	)
+
+	if errSend != nil {
+		return
+	}
+}
+
+func SendMail(mailFrom, mailTo, key, host, addr, subject string, err error) {
+	auth := smtp.PlainAuth(
+		"",
+		mailFrom,
+		key,
+		host,
+	)
+
+	msg := "Subject: " + subject + "\n" + err.Error()
+
+	errSend := smtp.SendMail(
+		addr,
 		auth,
 		mailFrom,
 		[]string{mailTo},
